a9b8fe578a6098052cd5e70b2077ad720453d615
louhab abderazzak <louhab.abderazzak.pro@gmail.com>
2025-02-09 20:27:02 +0100

    singup page is done

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config()
 const app = express();
 import  authRoutes from '../routes/auth.route.js';
@@ -8,6 +9,12 @@ import messageRoutes from '../routes/message.route.js';
 import { connectDB } from "../lib/db.js";
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors(
+  {
+      origin : "http://localhost:5173",
+      credentials : true 
+  }
+));
 app.use('/api/auth' , authRoutes);
 app.use('/api/messages' , messageRoutes);
 
